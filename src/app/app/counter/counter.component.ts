d4af23dcc97ba17232b2f5ac5926e6364e4a3b2a
Schmitz Daniel <daniel.schmitz@schmitz-dynamics.ch>
2019-09-02 06:45:45 +0200

    first commit

--- src/app/app/counter/counter.component.ts
+++ src/app/app/counter/counter.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
 
 @Component({
-  selector: 'sd-counter',
-  templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
-})
-export class CounterComponent implements OnInit {
 
-  constructor() { }
+  selector: 'pg-counter',
+
+  templateUrl: './counter.component.html',
+
+  styleUrls: ['./counter.component.scss']
+
+})
+
+export class CounterComponent implements OnInit {
+  @Input() max: number = 100;
+  @Input() min: number = 0;
+  @Input() stepSize: number = 1;
+  @Output() decreased: EventEmitter<number> = new EventEmitter();
+  @Output() increased: EventEmitter<number> = new EventEmitter();
+  public count: number = 0;
+
+  public increase() {
+    if (this.count + this.stepSize > this.max) {
+      this.count = this.max;
+    } else {
+      this.count += this.stepSize;
+    }
+    this.increased.emit(this.count);
+  }
+
+
+  public decrease() {
+    if (this.count - this.stepSize < this.min) {
+      this.count = this.min;
+    } else {
+      this.count -= this.stepSize;
+    }
+    this.decreased.emit(this.count);
+
+  }
+
+
+  constructor() {
+
+  }
+
 
   ngOnInit() {
+
   }
+
 
 }
